0f35a4a5758f5cd20955e35f6b7093a7bb808e93
Saurav Kumar <maxsaurav237@gmail.com>
2023-01-19 22:01:34 +0530

    style: skeleton, filter, read and unread emails

--- src/components/EmailItem/EmailItem.jsx
+++ src/components/EmailItem/EmailItem.jsx
@@ -1,21 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { formatDate } from "../../helpers/formatDate";
 import styles from "./EmailItem.module.css";
 
 export function EmailItem({ item }) {
+  const { readIds } = useSelector((state) => state.emails);
   const {
-    short_description,
+    id,
     subject,
+    short_description,
     from: { name, email },
   } = item;
   return (
-    <div className={styles.emailItemContainer}>
+    <div
+      className={
+        readIds.includes(id) ? styles.readContainer : styles.unReadContainer
+      }
+    >
       <div className={styles.avatar}>F</div>
       <div className={styles.detailsContainer}>
         <div className={styles.from}>
           <span>From:</span>
-          <span className={styles.emailId}>
-            {name} {email}
-          </span>
+          <div className={styles.emailId}>
+            <span className={styles.name}>{name}</span>{" "}
+            <span>{"<" + email + ">"}</span>
+          </div>
         </div>
         <div className={styles.subject}>
           <span>Subject:</span>
@@ -25,7 +34,7 @@ export function EmailItem({ item }) {
           <p>{short_description}</p>
         </div>
         <div className={styles.otherDetails}>
-          <span>23/12/2022 10:30am</span>
+          <span>{formatDate(item.date)}</span>
           <span className={styles.favorite}>Favorite</span>
         </div>
       </div>
---
+++ src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Skeleton.module.css";
+
+export function Skeleton() {
+  return <div className={styles.container}></div>;
+}
